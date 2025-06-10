9a89c6ba5f71366d11e55c3b68731b3c3d74073d
Raraandinii <raraandinib@gmail.com>
2025-06-10 18:29:38 +0700

    menunjuk ke sebuah file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -26,4 +26,7 @@ int main() {
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
     //Membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    
 }
