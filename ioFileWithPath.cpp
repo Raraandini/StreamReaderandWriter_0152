f927db7124f24598109acc078b103e7555baf00a
Raraandinii <raraandinib@gmail.com>
2025-06-10 18:30:00 +0700

    jika file ada maka

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -28,5 +28,9 @@ int main() {
     //Membuka file dalam mode membaca
     ifstream infile;
     //menunjuk ke sebuah file
+    infile.open(NamaFile, ios::in);
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
     
 }
