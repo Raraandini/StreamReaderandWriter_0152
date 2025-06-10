3eed07b5919f0b79d43363a425dde003c7e68874
Raraandinii <raraandinib@gmail.com>
2025-06-10 18:05:34 +0700

    melakukan perulangan setiap baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -29,5 +29,9 @@ int main() {
     infile.open("contohfile.txt");
     cout << endl << ">= Membuka dan membaca file " << endl;
     //jika file ada maka
-    
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        
+
 }
